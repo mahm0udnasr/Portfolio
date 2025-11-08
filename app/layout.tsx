3c770fc76ec0aa3d8ef23d971c6ad75fb47eed5d
mahm0udnasr <contact.mahmoudnasr@gmail.com>
2025-11-08 14:37:59 +0200

    comment navbar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins, Rubik } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -61,6 +62,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${poppins.variable} ${rubik.variable} antialiased`}>
+        {/* <Navbar /> */}
         {children}
       </body>
     </html>
