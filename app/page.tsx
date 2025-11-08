04b81038ba16bb2841064dc16617750617fd3646
mahm0udnasr <contact.mahmoudnasr@gmail.com>
2025-11-08 13:35:44 +0200

    make a site responseve

--- app/page.tsx
+++ app/page.tsx
@@ -9,11 +9,11 @@ export default function Home() {
         <h3 className="text-white font-poppins text-2xl max-sm:text-xl">
           My Name Is
         </h3>
-        <h1 className="block text-white font-rubik text-8xl max-sm:text-6xl">
+        <h1 className="block text-white font-rubik text-8xl max-sm:text-4xl">
           MahmoudNasr<span className="font-poppins text-orange-500">.</span>
         </h1>
         <div className="py-4 rounded-md flex flex-col justify-center items-center overflow-hidden">
-          <div className="font-poppins text-base sm:text-2xl text-balance text-gray-400">
+          <div className="font-poppins text-base sm:text-2xl max-sm:flex max-sm:flex-col text-balance text-gray-400">
             I am a Freelancer &{" "}
             <span className="font-rubik text-lg sm:text-3xl leading-tight text-orange-500">
               <TextEffect />
@@ -26,7 +26,7 @@ export default function Home() {
           <a
             href="https://wa.me/+201289950555"
             target="_blank"
-            className="group relative inline-flex items-center justify-center px-8 py-3.5 text-base font-semibold text-orange-500 transition-all duration-300 ease-out border-2 border-orange-500 rounded-lg bg-transparent hover:bg-orange-500 hover:text-black hover:shadow-[0_0_20px_rgba(249,115,22,0.4)] active:scale-95 overflow-hidden"
+            className="group relative max-sm:w-full max-sm:mx-7 inline-flex items-center justify-center px-8 py-3.5 text-base font-semibold text-orange-500 transition-all duration-300 ease-out border-2 border-orange-500 rounded-lg bg-transparent hover:bg-orange-500 hover:text-black hover:shadow-[0_0_20px_rgba(249,115,22,0.4)] active:scale-95 overflow-hidden"
           >
             <span className="relative z-10 flex items-center gap-2">
               Hire Me
@@ -37,7 +37,7 @@ export default function Home() {
           <a
             href="/Mahmoud Nasr _Front-end Developer _resume.pdf"
             download={true}
-            className="group relative inline-flex items-center justify-center px-8 py-3.5 text-base font-semibold text-black bg-orange-500 rounded-lg transition-all duration-300 ease-out hover:bg-transparent hover:text-orange-500 border-2 border-orange-500 hover:shadow-[0_0_20px_rgba(249,115,22,0.4)] active:scale-95 overflow-hidden"
+            className="group relative max-sm:w-full max-sm:mx-7 inline-flex items-center justify-center px-8 py-3.5 text-base font-semibold text-black bg-orange-500 rounded-lg transition-all duration-300 ease-out hover:bg-transparent hover:text-orange-500 border-2 border-orange-500 hover:shadow-[0_0_20px_rgba(249,115,22,0.4)] active:scale-95 overflow-hidden"
           >
             <span className="relative z-10 flex items-center gap-2">
               <Download size={18} />
