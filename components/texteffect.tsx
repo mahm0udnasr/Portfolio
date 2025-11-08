49fff495e50c8808bdd5d3bdb85e1ff5c66a9532
mahm0udnasr <contact.mahmoudnasr@gmail.com>
2025-11-08 13:36:10 +0200

    change text

--- components/texteffect.tsx
+++ components/texteffect.tsx
@@ -4,7 +4,7 @@ import { TypeAnimation } from "react-type-animation";
 export default function TextEffect() {
   return (
     <TypeAnimation
-      sequence={["Next.js Developer", 1000, "MERN Stack Developer", 1000]}
+      sequence={["Next.js Developer", 1000, "MERN Developer", 1000]}
       wrapper="span"
       speed={25}
       repeat={Infinity}
