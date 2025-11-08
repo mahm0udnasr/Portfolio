984993c3abf8381ae7c332d2fadacc1d7599c891
mahm0udnasr <contact.mahmoudnasr@gmail.com>
2025-11-08 17:58:05 +0200

    change some styles

--- app/page.tsx
+++ app/page.tsx
@@ -11,11 +11,12 @@ export default function Home() {
         <h3 className="text-white font-poppins text-2xl max-sm:text-xl">
           My Name Is
         </h3>
-        <h1 className="block text-white font-rubik text-8xl max-sm:text-4xl">
-          MahmoudNasr<span className="font-poppins text-orange-500">.</span>
+        <h1 className="block text-white font-rubik text-8xl max-sm:text-5xl">
+          MahmoudNasr
+          {/* <span className="font-poppins text-orange-500  text-6xl">.</span> */}
         </h1>
         <div className="py-4 rounded-md flex flex-col justify-center items-center overflow-hidden">
-          <div className="font-poppins text-base sm:text-2xl max-sm:flex max-sm:flex-col text-balance text-gray-400">
+          <div className="font-poppins text-base sm:text-2xl max-sm:flex max-sm:flex-col text-balance text-gray-300">
             I am a Freelancer &{" "}
             <span className="font-rubik text-lg sm:text-3xl leading-tight text-orange-500">
               <TextEffect />
@@ -31,8 +32,8 @@ export default function Home() {
             className="group relative max-sm:w-full max-sm:mx-7 inline-flex items-center justify-center px-8 py-3.5 text-base font-semibold text-orange-500 transition-all duration-300 ease-out border-2 border-orange-500 rounded-lg bg-transparent hover:bg-orange-500 hover:text-black hover:shadow-[0_0_20px_rgba(249,115,22,0.4)] active:scale-95 overflow-hidden"
           >
             <span className="relative z-10 flex items-center gap-2">
-              <Download size={18} />
               Download Resume
+              <Download size={18} />
             </span>
           </a>
 
