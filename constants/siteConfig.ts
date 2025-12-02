24c7ab34311fd96aa30451fd3ed9e3fad132cf3e
mahm0udnasr <contact.mahmoudnasr@gmail.com>
2025-12-02 22:09:51 +0200

    feat: add site configuration with skills and deployment options

---
+++ constants/siteConfig.ts
@@ -0,0 +1,33 @@
+export const siteConfig = {
+  name: "Mahmoud Nasr",
+  skills: {
+    "Programming Languages": [
+      "HTML5",
+      "CSS3",
+      "JavaScript",
+      "TypeScript",
+      "C++",
+    ],
+    "Framework/Libraries": [
+      "React",
+      "Next.js",
+      "Express",
+      "Tailwind CSS",
+      "shadcn/ui",
+      "clerk",
+      "NextAuth",
+      "prisma",
+      "more...",
+    ],
+    "Tools & Technologies": [
+      "Git",
+      "Github",
+      "VS Code",
+      "WebStorm",
+      "Postman",
+      "MongoDB",
+      "PostgreSQL",
+    ],
+    deploy: ["Vercel", "Netlify", "Heroku"],
+  },
+};
