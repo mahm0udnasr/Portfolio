a16063d4759dcb31faa8c3b5a411d37d82bb4a63
mahm0udnasr <contact.mahmoudnasr@gmail.com>
2025-12-04 07:27:22 +0200

    feat: update layout components to enhance styling and include Navbar

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,16 +1,10 @@
-
-
-
 export default function PagesLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
   return (
-    <div
-      className={`h-full w-full relative flex flex-col items-start gap-5 overflow-hidden pt-24 pb-4 px-40 mx-md:p-4 max-sm:pt-20`}
-    >
+    <div className="min-h-screen flex flex-col justify-center max-w-7xl mx-auto px-6 py-20 sm:px-8 lg:px-12">
       {children}
     </div>
   );
