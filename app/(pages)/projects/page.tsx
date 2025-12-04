a06c34c2972052e11254d485d988206f07055b3c
mahm0udnasr <contact.mahmoudnasr@gmail.com>
2025-12-04 07:27:52 +0200

    feat: add project images and documentation for Project Card component

--- app/(pages)/projects/page.tsx
+++ app/(pages)/projects/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Metadata } from "next";
-import Badge from "../badge";
 import { Layers } from "lucide-react";
+import HeaderPage from "../components/header";
 
 export const metadata: Metadata = {
   title: "Projects",
@@ -19,18 +19,14 @@ const ProjectsPage = () => {
   return (
     // <div className="flex relative wrap-break-word min-h-screen h-dvh items-center justify-between pt-14 pb-4 px-40 mx-md:p-4 max-sm:pt-20">
     <div>
-      <div>
-        <Badge Icon={Layers} title="Projects" />
-      </div>
-      <h1 className="mt-2.5 font-bold text-3xl text-secondary relative after:content-[''] after:absolute after:left-0 after:-bottom-3 after:w-20 after:h-2 after:bg-orange-500">
-        My Projects
-      </h1>
-      <p className="text-secondary/80 mt-4 leading-7">
-        I love to Build Cool Projects. Here, you'll find a curated collection of
+      <HeaderPage
+        Icon={Layers}
+        title="My Projects"
+        description="        I love to Build Cool Projects. Here, you'll find a curated collection of
         my creative endeavors and technical projects. Each piece represents a
         journey of innovation, problem-solving, and continuous learning. Feel
-        free to explore this showcase of my passion and expertise in action.
-      </p>
+        free to explore this showcase of my passion and expertise in action."
+      />
     </div>
   );
 };
---
+++ app/(pages)/projects/project-card.md
@@ -0,0 +1,18 @@
+# UI design for Project Card Component from replit.com
+
+[project card](https://replit.com/@mahm0udnasr/CardShowcase)
+
+# images
+
+1 - without hover effect
+![alt text](image.png)
+
+2 - with hover effect on image
+![alt text](image-1.png)
+
+3 - with hover effect on button
+![alt text](image-2.png)
+
+# Notes
+
+- add a source code button
