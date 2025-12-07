f5721f773c18fecdbe6664b9a15ab5acbbf84647
mahm0udnasr <contact.mahmoudnasr@gmail.com>
2025-12-07 09:17:52 +0200

    make header more useful

--- app/(pages)/components/header.tsx
+++ app/(pages)/components/header.tsx
@@ -5,18 +5,20 @@ import Title from "./title";
 interface HeaderPageProps {
   Icon: LucideIcon;
   title: string;
+  badgeTitle: string;
   description: string;
 }
 
 export default function HeaderPage({
   Icon,
   title,
+  badgeTitle,
   description,
 }: HeaderPageProps) {
   return (
     <>
       <div>
-        <Badge Icon={Icon} title="About me" />
+        <Badge Icon={Icon} title={badgeTitle} />
       </div>
       <Title title={title} />
       <p className="text-secondary/80 mt-4 leading-7">{description}</p>
