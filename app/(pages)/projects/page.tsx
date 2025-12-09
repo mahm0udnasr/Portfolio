5afce9fb7ca5745fdfeff6675432c3e656a98145
mahm0udnasr <contact.mahmoudnasr@gmail.com>
2025-12-09 21:18:15 +0200

    enhance ui design for project page

--- app/(pages)/projects/page.tsx
+++ app/(pages)/projects/page.tsx
@@ -3,6 +3,7 @@ import { Metadata } from "next";
 import { Layers } from "lucide-react";
 import HeaderPage from "../components/header";
 import Projects from "./projects";
+import Project from "./project";
 
 export const metadata: Metadata = {
   title: "Projects",
@@ -17,6 +18,38 @@ export const metadata: Metadata = {
 };
 
 const ProjectsPage = () => {
+  const projectsData = [
+    {
+      img: "https://i.ibb.co/M5s2R37d/dashboard.webp",
+      title: "Nexus Analytics1",
+      description:
+        "A comprehensive analytics suite featuring real-time data visualization and customizable widget layouts for enterprise applications.",
+      skills: ["React", "D3.js", "Tailwind CSS"],
+      demoLink: "https://nexus-analytics-demo.com",
+      sourceCodeLink: "https://google.com",
+      priority: 1,
+    },
+    {
+      img: "https://i.ibb.co/M5s2R37d/dashboard.webp",
+      title: "Nexus Analytics2",
+      description:
+        "A comprehensive analytics suite featuring real-time data visualization and customizable widget layouts for enterprise applications.",
+      skills: ["React", "D3.js", "Tailwind CSS"],
+      demoLink: "https://nexus-analytics-demo.com",
+      sourceCodeLink: "https://google.com",
+      priority: 2,
+    },
+    {
+      img: "https://i.ibb.co/M5s2R37d/dashboard.webp",
+      title: "Nexus Analytics3",
+      description:
+        "A comprehensive analytics suite featuring real-time data visualization and customizable widget layouts for enterprise applications.",
+      skills: ["React", "D3.js", "Tailwind CSS"],
+      demoLink: "https://nexus-analytics-demo.com",
+      sourceCodeLink: "https://google.com",
+      priority: 1,
+    },
+  ];
   return (
     <div>
       <HeaderPage
@@ -28,7 +61,20 @@ const ProjectsPage = () => {
         journey of innovation, problem-solving, and continuous learning. Feel
         free to explore this showcase of my passion and expertise in action."
       />
-      <Projects />
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 md:gap-8 px-4 md:px-6 lg:px-8 py-8 md:py-12">
+        {projectsData.sort((a, b) => a.priority - b.priority).map((project, index) => (
+          <Project
+            key={index}
+            img={project.img}
+            title={project.title}
+            description={project.description}
+            skills={project.skills}
+            demoLink={project.demoLink}
+            sourceCodeLink={project.sourceCodeLink}
+            priority={project.priority}
+          />
+        ))}
+      </div>
     </div>
   );
 };
